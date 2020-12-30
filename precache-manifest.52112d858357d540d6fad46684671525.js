c7a3e7c459810dfeda7ea5287e0277bb0451c69b
gvemulapalli33 <gvemulapalli33@gmail.com>
2020-12-30 13:59:34 -0800

    Updates

---
+++ precache-manifest.52112d858357d540d6fad46684671525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbfaadc3ee2804c306f87a1ec14d8204",
+    "url": "//gvemulapalli33.github.io/moviesList/index.html"
+  },
+  {
+    "revision": "bf124bb0789d2ce19435",
+    "url": "//gvemulapalli33.github.io/moviesList/static/css/main.10cc49bc.chunk.css"
+  },
+  {
+    "revision": "d66c37b44b67fe7c81a5",
+    "url": "//gvemulapalli33.github.io/moviesList/static/js/2.2d4e8886.chunk.js"
+  },
+  {
+    "revision": "b3c110dea661c3006c81",
+    "url": "//gvemulapalli33.github.io/moviesList/static/js/3.4f686063.chunk.js"
+  },
+  {
+    "revision": "bf124bb0789d2ce19435",
+    "url": "//gvemulapalli33.github.io/moviesList/static/js/main.d279a263.chunk.js"
+  },
+  {
+    "revision": "50bb50cad4222a682c32",
+    "url": "//gvemulapalli33.github.io/moviesList/static/js/runtime~main.f304e4d5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/moviesList/precache-manifest.7b312a8c0ba5ff59c0f5d52b2efe565d.js"
+  "//gvemulapalli33.github.io/moviesList/precache-manifest.52112d858357d540d6fad46684671525.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/moviesList/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//gvemulapalli33.github.io/moviesList/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
